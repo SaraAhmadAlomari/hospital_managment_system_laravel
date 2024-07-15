205a6edc6b8457303ab90370acc236894c49f17c
SaraAhmadAlomari <sarhmry047@gmail.com>
2024-07-15 17:54:16 +0300

    change login img

--- resources/views/Dashboard/User/auth/signin.blade.php
+++ resources/views/Dashboard/User/auth/signin.blade.php
@@ -11,10 +11,10 @@
 		<div class="container-fluid">
 			<div class="row no-gutter">
 				<!-- The image half -->
-				<div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
+				<div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-white">
 					<div class="row wd-100p mx-auto text-center">
 						<div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
-							<img src="{{URL::asset('Dashboard/img/media/login.png')}}" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
+							<img src="{{URL::asset('Dashboard/img/media/login.png')}}" class="my-auto cover" alt="logo">
 						</div>
 					</div>
 				</div>
@@ -26,7 +26,6 @@
 							<div class="row">
 								<div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
 									<div class="card-sigin">
-										<div class="mb-5 d-flex"> <a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('Dashboard/img/brand/favicon.png')}}" class="sign-favicon ht-40" alt="logo"></a><h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Va<span>le</span>x</h1></div>
 										<div class="card-sigin">
 											<div class="main-signup-header">
 												<h2>{{trans('Dashboard/login_trans.Welcome')}}</h2>
